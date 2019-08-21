a4dcc84c2cd6f6bf7dd90da1cb66d85c55bcfa96
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-08-21 15:57:22 -0400

    functionally components, import and exporting

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Welcomemessage from "./Welcomemessage";
 
 class App extends Component {
   render() {
-    return <div>hello world</div>;
+    return <Welcomemessage />;
   }
 }
 
