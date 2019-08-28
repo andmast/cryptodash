84aa6c4dcf6a7bf73ddc941ec785df6ed55f0823
Andrea Mastrantoni <anmastrantoni@gmail.com>
2019-08-28 14:35:48 -0400

    Styled AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,43 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
 
 const Bar = styled.div`
   display: grid;
+  margin-bottom: 40px;
   grid-template-columns: 180px auto 100px 100px;
 `;
+const ControllerButtonElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControllerButton({ name, active }) {
+  return (
+    <ControllerButtonElem active={active}>
+      {toProperCase(name)}
+    </ControllerButtonElem>
+  );
+}
 
 export default function() {
   return (
     <Bar>
-      <div>Child 1</div>
+      <Logo>CryptoDash</Logo>
       <div />
-      <div>Child 2</div>
-      <div>Child 3</div>
+      <ControllerButton active name="dashboard" />
+      <ControllerButton name="settings" />
     </Bar>
   );
 }
